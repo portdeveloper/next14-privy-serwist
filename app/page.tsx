f598cdc3cee8f0102309a3d8991c36e07813c6c7
portdeveloper <108868128+portdeveloper@users.noreply.github.com>
2025-06-14 22:24:02 +0300

    Add more Serwist stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import type { Metadata } from "next";
+
+import SendNotification from "./SendNotification";
+import { InstallPWA } from "./InstallPWA";
 import UseLoginPrivy from "./Privy";
 
-export default function Home() {
+export const metadata: Metadata = {
+  title: "Home",
+};
+
+export default function Page() {
   return (
-   <UseLoginPrivy />
+    <>
+      <h1>Next.js + Serwist</h1>
+      <InstallPWA />
+      <SendNotification />
+      <UseLoginPrivy />
+    </>
   );
 }
